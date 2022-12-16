a5c9ebf345806d54a454e555bf19c8fd285349f2
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-12-16 20:58:31 -0300

    build: mongoose setup

--- server/index.js
+++ server/index.js
@@ -33,3 +33,13 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+// Mongoose setup
+const PORT = process.env.PORT || 6001;
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+}).catch((error) => console.log(error.message));
