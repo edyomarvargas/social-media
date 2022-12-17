29e9e8b01b0a1f95ca7d0dc7a98340f01c0ae14c
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-12-17 10:16:35 -0300

    feat: create POST /auth/login

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import register from './controllers/auth.js';
+import { register } from './controllers/auth.js';
+import authRoutes from './routes/index.js';
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -36,7 +37,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Routes with files
-app.post('auth/register', upload.single('picture'), register);
+app.post('/auth/register', upload.single('picture'), register);
+
+// Routes
+app.use('/auth', authRoutes);
 
 // Mongoose setup
 const PORT = process.env.PORT || 6001;
