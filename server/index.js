67545964d26c4c4a96769c281fc795a690bb42ca
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-12-17 13:46:53 -0300

    feat: create GET, POST and PATCH requests in /posts

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { register } from './controllers/auth.js';
+import { createPost } from './controllers/post.js';
 import authRoute from './routes/auth.js';
 import usersRoute from './routes/users.js';
+import postRoute from './routes/posts.js';
+import verifyToken from './middleware/auth.js';
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -39,10 +42,12 @@ const upload = multer({ storage });
 
 // Routes with files
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
 
 // Routes
 app.use('/auth', authRoute);
 app.use('/users', usersRoute);
+app.use('/posts', postRoute);
 
 // Mongoose setup
 const PORT = process.env.PORT || 6001;
